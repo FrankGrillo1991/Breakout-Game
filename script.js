ae29a2d36d92c51af51e1e186b2c996304c3a5cf
FrankGrillo1991 <fsg12491@aol.com>
2025-05-08 08:27:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,23 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                const brickX = 
+                    c * (brickWidth + brickPadding) + brickOffsetLeft;
+                const brickY =
+                    r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#00ff99";
+                ctx.closePath();
+            }
+        }
+    }
+}
+
