fac2e31bfb36dde203f5b84afef536b8af2cf601
FrankGrillo1991 <fsg12491@aol.com>
2025-05-08 07:58:27 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("breakout");
+const ctx = canvas.getContext("2d");
+
+const ballRadius = 10;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+let score = 0;
+let lives = 3;
